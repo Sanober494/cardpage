53a2ff09ac93e15dbde065034838ac41f63ae137
Sanober494 <75628824+Sanober494@users.noreply.github.com>
2023-07-08 10:34:25 +0400

    Update profile.dart

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -19,6 +19,10 @@ class EditProfilePage extends StatefulWidget {
 
 class _EditProfilePageState extends State<EditProfilePage> {
   bool showPassword = false;
+  String panCardErrorText = '';
+  String passwordErrorText = '';
+  String selectedDate = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,9 +49,7 @@ class _EditProfilePageState extends State<EditProfilePage> {
                 "Profile Set-Up",
                 style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
               ),
-              SizedBox(
-                height: 15,
-              ),
+              SizedBox(height: 15),
               Center(
                 child: Stack(
                   children: [
@@ -55,57 +57,58 @@ class _EditProfilePageState extends State<EditProfilePage> {
                       width: 130,
                       height: 130,
                       decoration: BoxDecoration(
-                          border: Border.all(
-                              width: 4,
-                              color: Theme.of(context).scaffoldBackgroundColor),
-                          boxShadow: [
-                            BoxShadow(
-                                spreadRadius: 2,
-                                blurRadius: 10,
-                                color: Colors.black.withOpacity(0.1),
-                                offset: Offset(0, 10))
-                          ],
-                          shape: BoxShape.circle,
-                          image: DecorationImage(
-                              fit: BoxFit.cover,
-                              image: NetworkImage(
-                                "https://static.vecteezy.com/system/resources/previews/005/544/718/original/profile-icon-design-free-vector.jpg",
-                              ))),
+                        border: Border.all(
+                          width: 4,
+                          color: Theme.of(context).scaffoldBackgroundColor,
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            spreadRadius: 2,
+                            blurRadius: 10,
+                            color: Colors.black.withOpacity(0.1),
+                            offset: Offset(0, 10),
+                          ),
+                        ],
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(
+                            "https://static.vecteezy.com/system/resources/previews/005/544/718/original/profile-icon-design-free-vector.jpg",
+                          ),
+                        ),
+                      ),
                     ),
                     Positioned(
-                        bottom: 0,
-                        right: 0,
-                        child: Container(
-                          height: 40,
-                          width: 40,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            border: Border.all(
-                              width: 4,
-                              color: Theme.of(context).scaffoldBackgroundColor,
-                            ),
-                            color: Colors.green,
+                      bottom: 0,
+                      right: 0,
+                      child: Container(
+                        height: 40,
+                        width: 40,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            width: 4,
+                            color: Theme.of(context).scaffoldBackgroundColor,
                           ),
-                          child: Icon(
-                            Icons.edit,
-                            color: Colors.white,
-                          ),
-                        )),
+                          color: Colors.green,
+                        ),
+                        child: Icon(
+                          Icons.edit,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
                   ],
                 ),
               ),
-              SizedBox(
-                height: 35,
-              ),
+              SizedBox(height: 35),
               buildTextField("Full Name", "example: Adam Alex", false),
               buildTextField("E-mail", "example: adama@gmail.com", false),
-              buildTextField("Password", "Create password", true),
+              buildPasswordField(),
               buildTextField("Address", "example: Mumbai, India", false),
-              buildTextField("Date of Birth", "example: 23-04-1981", false),
-              buildTextField("PAN Card Number", "example: ABC1234XXX", false),
-              SizedBox(
-                height: 35,
-              ),
+              buildDatePickerField(),
+              buildPANCardField(),
+              SizedBox(height: 35),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -167,28 +170,155 @@ class _EditProfilePageState extends State<EditProfilePage> {
       child: TextField(
         obscureText: isPasswordTextField ? showPassword : false,
         decoration: InputDecoration(
-            suffixIcon: isPasswordTextField
-                ? IconButton(
-                    onPressed: () {
-                      setState(() {
-                        showPassword = !showPassword;
-                      });
-                    },
-                    icon: Icon(
-                      Icons.remove_red_eye,
-                      color: Colors.grey,
-                    ),
-                  )
-                : null,
-            contentPadding: EdgeInsets.only(bottom: 3),
-            labelText: labelText,
-            floatingLabelBehavior: FloatingLabelBehavior.always,
-            hintText: placeholder,
-            hintStyle: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-            )),
+          suffixIcon: isPasswordTextField
+              ? IconButton(
+                  onPressed: () {
+                    setState(() {
+                      showPassword = !showPassword;
+                    });
+                  },
+                  icon: Icon(
+                    Icons.remove_red_eye,
+                    color: Colors.grey,
+                  ),
+                )
+              : null,
+          contentPadding: EdgeInsets.only(bottom: 3),
+          labelText: labelText,
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          hintText: placeholder,
+          hintStyle: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Colors.black54, // Changed to light grey color
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildPasswordField() {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 35.0),
+      child: TextField(
+        obscureText: !showPassword,
+        decoration: InputDecoration(
+          suffixIcon: IconButton(
+            onPressed: () {
+              setState(() {
+                showPassword = !showPassword;
+              });
+            },
+            icon: Icon(
+              Icons.remove_red_eye,
+              color: Colors.grey,
+            ),
+          ),
+          contentPadding: EdgeInsets.only(bottom: 3),
+          labelText: "Password",
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          hintText: "Create password",
+          hintStyle: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Colors.black54, // Changed to light grey color
+          ),
+          errorText: passwordErrorText.isNotEmpty ? passwordErrorText : null,
+        ),
+        onChanged: (value) {
+          setState(() {
+            // Perform password validation using regular expressions
+            RegExp regex = RegExp(
+              r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9!#%]).{8,}$',
+            );
+            if (!regex.hasMatch(value)) {
+              passwordErrorText = "The Password should contain: \n"
+                  "An uppercase character (A-Z)\n"
+                  "A lowercase character (a-z)\n"
+                  "A number (0-9) and/or symbol (such as !, #, or %)\n"
+                  "8 or more characters total";
+            } else {
+              passwordErrorText = '';
+            }
+          });
+        },
+      ),
+    );
+  }
+
+  Widget buildDatePickerField() {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 35.0),
+      child: GestureDetector(
+        onTap: () {
+          showDatePicker(
+            context: context,
+            initialDate: DateTime.now(),
+            firstDate: DateTime(1900),
+            lastDate: DateTime.now(),
+          ).then((selectedDate) {
+            setState(() {
+              if (selectedDate != null) {
+                this.selectedDate = selectedDate.toString();
+              }
+            });
+          });
+        },
+        child: AbsorbPointer(
+          child: TextField(
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.only(bottom: 3),
+              labelText: "Date of Birth",
+              floatingLabelBehavior: FloatingLabelBehavior.always,
+              hintText: selectedDate.isNotEmpty
+                  ? selectedDate
+                  : "example: 23-04-1981",
+              hintStyle: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.black54, // Changed to light grey color
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildPANCardField() {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 35.0),
+      child: TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.only(bottom: 3),
+          labelText: "PAN Card Number",
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          hintText: "example: ABC1234XXX",
+          hintStyle: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Colors.black54, // Changed to light grey color
+          ),
+          errorText: panCardErrorText.isNotEmpty ? panCardErrorText : null,
+        ),
+        onChanged: (value) {
+          setState(() {
+            // Perform PAN card number validation using regular expressions
+            RegExp regex = RegExp(
+              r'^[A-Z]{5}[0-9]{4}[A-Z]{1}$',
+            );
+            if (!regex.hasMatch(value)) {
+              panCardErrorText = "The PAN Card Number should be:\n"
+                  "Ten characters long.\n"
+                  "The first five characters should be any uppercase alphabets.\n"
+                  "The next four characters should be any number from 0 to 9.\n"
+                  "The last (tenth) character should be any uppercase alphabet.\n"
+                  "It should not contain any white spaces.";
+            } else {
+              panCardErrorText = '';
+            }
+          });
+        },
       ),
     );
   }
